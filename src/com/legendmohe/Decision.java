1a1fd896a8ef6c7d956fba781864ee6e03e739de
hexinyu <hexinyu@bigo.sg>
2019-05-31 11:56:51 +0800

    Initial commit

---
+++ src/com/legendmohe/Decision.java
@@ -0,0 +1,88 @@
+package com.legendmohe;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 适用场景：各个模块拦截处理touch事件等
+ *
+ * Created by hexinyu on 2019/5/31.
+ */
+public class Decision {
+
+    private static class LazyHolder {
+        private static final Decision INSTANCE = new Decision();
+    }
+
+    private static Decision getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private Map<String, Maker> mHolderMap = new HashMap<String, Maker>();
+
+    public synchronized static Maker forEvent(String eventName) {
+        if (!getInstance().mHolderMap.containsKey(eventName)) {
+            getInstance().mHolderMap.put(eventName, new Maker());
+        }
+        return getInstance().mHolderMap.get(eventName);
+    }
+
+    public synchronized static void remove(String makerName) {
+        getInstance().mHolderMap.remove(makerName);
+    }
+
+    public synchronized static void clear() {
+        getInstance().mHolderMap.clear();
+    }
+
+    ///////////////////////////////////private///////////////////////////////////
+
+    public static class Maker {
+        List<MakeDecision> mDecisions = new CopyOnWriteArrayList<MakeDecision>();
+
+        public Maker add(MakeDecision decision) {
+            mDecisions.add(decision);
+            return this;
+        }
+
+        public Maker remove(MakeDecision decision) {
+            mDecisions.remove(decision);
+            return this;
+        }
+
+        public <E, R> R make(E event) {
+            Collections.sort(mDecisions, new Comparator<MakeDecision>() {
+                @Override
+                public int compare(MakeDecision o1, MakeDecision o2) {
+                    return o2.getPriority() - o1.getPriority();
+                }
+            });
+            R result = null;
+            for (MakeDecision<E, R> decision : mDecisions) {
+                result = decision.onEvent(event);
+                if (result != null) {
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public Maker clear() {
+            mDecisions.clear();
+            return this;
+        }
+    }
+
+    public interface MakeDecision<E, R> {
+
+        // return null to invoke next processor
+        R onEvent(E event);
+
+        // higher priority gets first
+        int getPriority();
+    }
+}
---
+++ src/com/legendmohe/Main.java
@@ -0,0 +1,55 @@
+package com.legendmohe;
+
+import javafx.scene.input.TouchEvent;
+
+public class Main {
+
+    public static final int PRIORITY_HIGH = 3;
+    public static final int PRIORITY_DEFAULT = 2;
+    public static final int PRIORITY_LOW = 1;
+
+    public static void main(String[] args) {
+        Decision.forEvent("touch").add(new Decision.MakeDecision<TouchEvent, Integer>() {
+            @Override
+            public Integer onEvent(TouchEvent event) {
+                return null;
+            }
+
+            @Override
+            public int getPriority() {
+                return PRIORITY_HIGH;
+            }
+        });
+
+        Decision.forEvent("touch").add(new Decision.MakeDecision<TouchEvent, Integer>() {
+            @Override
+            public Integer onEvent(TouchEvent event) {
+                return 2;
+            }
+
+            @Override
+            public int getPriority() {
+                return PRIORITY_DEFAULT;
+            }
+        });
+
+        Decision.forEvent("touch").add(new Decision.MakeDecision<TouchEvent, Integer>() {
+            @Override
+            public Integer onEvent(TouchEvent event) {
+                return 1;
+            }
+
+            @Override
+            public int getPriority() {
+                return PRIORITY_LOW;
+            }
+        });
+
+        Integer result = Decision.forEvent("touch")
+                .make(new TouchEvent(null, null, null, -1, false, false, false, false));
+        System.out.println("result=" + result);
+
+        Decision.clear();
+    }
+
+}
